b87d2a10aaa687c8203ea2197a74c124efbe8576
AyoubAch15 <la217073@student.helha.be>
2023-05-29 15:50:56 +0200

    Add existing file

---
+++ Projet2/app/db/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'dbProj'
+  
+});
+
+// Connect to the database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected to the MySQL database');
+});
+
+module.exports = connection;
